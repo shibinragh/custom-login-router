3638bf56720ca8255a4beab1cc4e20e9a91c0d5a
shibinragh <shibin@toobler.com>
2018-08-27 17:32:40 +0530

    added express-session

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(session({secret: "secret!"}));
 app.use(bodyParser.urlencoded({extended:false}))
 
 var db = require('./db/db');
@@ -13,6 +15,17 @@ const router = require('./routers/index');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+app.get('/', function(req, res){
+    console.log(req.sessionID)
+    if(req.session.page_views){
+       req.session.page_views++;
+       res.send("You visited this page " + req.session.page_views + " times");
+    } else {
+       req.session.page_views = 1;
+       res.send("Welcome to this page for the first time!");
+    }
+ });
+ 
 app.use('/', router);
 
 app.listen(3000, console.log('port' + 3000));
